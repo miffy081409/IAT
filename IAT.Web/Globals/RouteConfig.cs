ef7fb90abb6274d36f2d94005d680722ba867f38
miffy081409 <miffy081409@gmail.com>
2015-10-30 19:21:06 +0800

    Upload web source code

---
+++ IAT.Web/Globals/RouteConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IAT.Web.Globals
+{
+    public class RouteConfig
+    {
+        public static void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc(routes => {
+                routes.MapRoute("Default", "{controller=home}/{action=index}/{id?}");
+            });
+        }
+    }
+}
---
+++ IAT.Web/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Configuration;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Dnx.Runtime;
+using IAT.Web.Globals;
+
+namespace IAT.Web
+{
+    public class Startup
+    {
+        public static IConfiguration Configuration { get; set; }
+
+        public Startup(IHostingEnvironment env, IApplicationEnvironment appEnv)
+        {
+            // Setup configuration sources.
+
+            var builder = new ConfigurationBuilder(appEnv.ApplicationBasePath).AddJsonFile("config.json").AddJsonFile($"config.{env.EnvironmentName}.json", optional: true);
+
+            //if (env.IsDevelopment())
+            //{
+            //    // This reads the configuration keys from the secret store.
+            //    // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
+            //    builder.AddUserSecrets();
+            //}
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            RouteConfig.Configure(app);
+        }
+    }
+}
