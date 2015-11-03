499ddc58de9eecf3a97289f72960707c1b74edef
miffy081409 <miffy081409@gmail.com>
2015-11-03 22:08:29 +0800

    update dashboard model

---
+++ IAT.Web/wwwroot/apps/Dashboard/Models/app.js
@@ -0,0 +1,10 @@
+﻿dashboardApp.service('app', function ($http) {
+
+    this.ValidateApp = function (appName, callback) {
+        $http.get('/api/app/' + appName).success(function (data) {
+            if (callback != undefined) {
+                callback(data);
+            }
+        });
+    }
+});
